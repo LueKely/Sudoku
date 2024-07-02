2ffabc45cfe43c3a733bfd71eb090aedaeaab44f
LueKely <luekely43110@gmail.com>
2024-07-02 09:55:06 +0800

    UI

--- main.js
+++ main.js
@@ -1,10 +1,51 @@
-import solveSudoku from "./assets/sudokuSolver";
-import Sudoku from "./assets/sudokus";
+import solveSudoku from './assets/sudokuSolver';
+import Sudoku from './assets/sudokus';
 
 let N = 9;
 let K = 40;
 let sudokuBoard = new Sudoku(N, K);
-
 sudokuBoard.fillValues();
-console.table(sudokuBoard.mat);
-console.table(solveSudoku(sudokuBoard.mat));
+
+const maskedBoard = structuredClone(sudokuBoard.mat);
+const solvedBoard = solveSudoku(sudokuBoard.mat);
+
+const AnswerButton = document.querySelector('.Answer');
+const GameButton = document.querySelector('.Game');
+
+// table > tbody
+let Board = document.getElementById('Board').children[0];
+injectMaskedBoard();
+
+AnswerButton.addEventListener('click', injectSolvedBoard);
+GameButton.addEventListener('click', () => {
+	window.location.reload();
+});
+
+// tr > td > input
+// console.log(Board.children[0].children[0].firstChild.value);
+
+function injectMaskedBoard() {
+	for (let index = 0; index < Board.childElementCount; index++) {
+		for (
+			let kindex = 0;
+			kindex < Board.children[index].childElementCount;
+			kindex++
+		) {
+			Board.children[index].children[kindex].firstChild.value =
+				maskedBoard[index][kindex];
+		}
+	}
+}
+
+function injectSolvedBoard() {
+	for (let index = 0; index < Board.childElementCount; index++) {
+		for (
+			let kindex = 0;
+			kindex < Board.children[index].childElementCount;
+			kindex++
+		) {
+			Board.children[index].children[kindex].firstChild.value =
+				solvedBoard[index][kindex];
+		}
+	}
+}
